7aa91b0f7a3d25b9e11d6be854b9ff058118e8a3
Lu-el <lubov.elunina@mail.ru>
2021-11-29 14:55:50 +0400

    task javaS 9.1

---
+++ Task9.1.js
@@ -0,0 +1,35 @@
+'use strict'
+let str = "Сосна";
+
+function lastVariant () {
+    cutExtraSymbols(input.value);
+    let div = document.createElement('div');
+    div.className = "task91";
+    div.innerHTML = `Решение: ${cutExtraSymbols(input.value)}`;
+    document.body.append(div);
+};
+
+function cutExtraSymbols (stroka) {
+  let newStr = Array.from(stroka);
+  for (let x=0; x <= (stroka.length-1); x++){
+      let massive1 = splitStrOnWords(stroka);
+      if ( massive1.includes( stroka[x].toLowerCase()) ) {
+          newStr.splice(x, 1, '');
+      }
+  } return  newStr.join('');
+};
+
+function splitStrOnWords(line) { // строка разбивается на слова и по каждому слову ищутся неуникальные символы
+    let ready = [];
+    let strWords = line.split(' '); 
+    for (let j=0; j <= (strWords.length-1); j++) {
+      let strSymbols = strWords[j].split(''); 
+      for (let i=0; i <= (strSymbols.length-1); i++){ 
+        let deleted = strSymbols.slice();
+        deleted.splice(i, 1);
+        if ( deleted.includes( strSymbols[i].toLowerCase()) ) {
+          ready.push( strSymbols[i].toLowerCase() );
+        }
+      } 
+    } return ready;
+};
